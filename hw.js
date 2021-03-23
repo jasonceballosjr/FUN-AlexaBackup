22ef42fe8c9a16652005a62b89796bf873c6347a
Jason Ceballos <78244336+jasonceballosjr@users.noreply.github.com>
2021-03-23 11:47:51 -0500

    Add files via upload

--- hw.js
+++ hw.js
@@ -6,7 +6,6 @@ let fortune = [
     "You're going places!", "You will see a blue bird."
 ];
 let ranNum = Math.floor(Math.random() * fortune.length);
-
 function myFunction() {
 
     if (prompt1 === decision[0]) {
@@ -15,6 +14,13 @@ function myFunction() {
         alert(user + "," + " you didn't deserve one anyways!");
     }
 }
-
-
 myFunction();
+
+/*Henry Video
+I found the protocol analogy where he used languages like English and spanish
+to explain that both ends needs to understand the requirements in order
+to communicate well.
+I also enjoyed how he explained back-end by using restaurants when your order
+had been placed and sent to the kitchen. What you get in return will be the
+pretty and tasty food (functioning website) after your request went through.
+*/
